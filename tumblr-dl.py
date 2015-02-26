9ff5cf75c607e27df15196fce75dde1770ce43b4
Kono <konosprod@free.fr>
2015-02-26 11:08:42 +0100

    Photoset downloader

--- tumblr-dl.py
+++ tumblr-dl.py
@@ -3,10 +3,32 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import sys
+import photoset
 
+rem_file = ""
+
+def dlProgress(count, blockSize, totalSize):
+    global rem_file
+    percent = int(count*blockSize*100/totalSize)
+    sys.stdout.write("\r"+ "Downloading " + rem_file + " ...%d%%" % percent)
+    sys.stdout.flush()
+    
+def getFilename(url):
+    return url[url.rfind('/')+1:]
+    
 
 def downloadTumblr(url):
-    print("here")
+    global rem_file
+    html = photoset.getSource(url);
+    photosets = html.findAll('iframe', attrs={'class' : 'photoset'})
+    for s in photosets:
+        imgUrl = photoset.getPhotosetImagesUrl(s['src'])
+        for u in imgUrl:
+            rem_file = getFilename(u)
+            urllib.request.urlretrieve(u, rem_file, reporthook=dlProgress)
+            print("")
+            
+        
 
 def printUsage():
     print("Usage : " + sys.argv[0] + " [tumblr url]")
